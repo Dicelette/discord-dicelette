34ee1989eb093ca0add7f7d2686e2e5aa75da20c
Mara <mara-li@outlook.fr>
2024-12-06 13:30:33 +0100

    test(vitest): add vitest config

--- packages/dice/vitest.config.ts
+++ packages/dice/vitest.config.ts
@@ -1,6 +1,13 @@
-import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
+import path from "node:path";
+import { defineProject } from "vitest/config";
 
-export default defineConfig({
-	plugins: [tsconfigPaths()],
+export default defineProject({
+	test: {
+		exclude: [],
+		alias: {
+			"@dicelette/localization": path.resolve(__dirname, "../localization/index.ts"),
+			"@dicelette/utils": path.resolve(__dirname, "../utils/index.ts"),
+			"@dicelette/types": path.resolve(__dirname, "../types/index.ts"),
+		},
+	},
 });
