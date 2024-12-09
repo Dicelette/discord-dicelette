a03f23bb218049ccef6d1e9eb2cc901323fee225
Lisandra Simonetti <l.simonetti@stragen.fr>
2024-12-09 11:22:33 +0100

    feat(logger): enhance logging configuration and add pretty log styles
    - Added default options for logger with pretty log templates and styles.
    - Included environment-specific log level settings for development and
      production.
    - Updated `important` logger configuration to include pretty log formats
      and styles.

--- packages/utils/src/logger.ts
+++ packages/utils/src/logger.ts
@@ -1,12 +1,76 @@
 import process from "node:process";
 import dotenv from "dotenv";
-import { type ILogObj, Logger } from "tslog";
+import { type ILogObj, type ISettingsParam, Logger } from "tslog";
 dotenv.config({ path: ".env" });
 
-const optionLoggers =
+const optionLoggers: ISettingsParam<ILogObj> =
 	process.env.NODE_ENV === "development"
-		? { minLevel: 0 }
-		: { minLevel: 4, hideLogPositionForProduction: true };
+		? {
+				minLevel: 0,
+			}
+		: {
+				minLevel: 4,
+				hideLogPositionForProduction: true,
+			};
 
-export const logger: Logger<ILogObj> = new Logger(optionLoggers);
-export const important: Logger<ILogObj> = new Logger({ name: "Note", minLevel: 0 });
+const defaultOptions = {
+	prettyLogTemplate:
+		"{{yyyy}}.{{mm}}.{{dd}} {{hh}}:{{MM}}:{{ss}}:{{ms}}\t{{logLevelName}}\t[{{filePathWithLine}}] ",
+	prettyErrorTemplate: "\n{{errorName}} {{errorMessage}}\nerror stack:\n{{errorStack}}",
+	prettyErrorStackTemplate: "  • {{fileName}}\t{{method}}\n\t{{filePathWithLine}}",
+	prettyErrorParentNamesSeparator: ":",
+	prettyErrorLoggerNameDelimiter: "\t",
+	stylePrettyLogs: true,
+	prettyLogTimeZone: "Europe/Paris",
+	prettyLogStyles: {
+		logLevelName: {
+			"*": ["bold", "black", "bgWhiteBright", "dim"],
+			SILLY: ["bold", "white"],
+			TRACE: ["bold", "whiteBright"],
+			DEBUG: ["bold", "green"],
+			INFO: ["bold", "blue"],
+			WARN: ["bold", "yellow"],
+			ERROR: ["bold", "red"],
+			FATAL: ["bold", "redBright"],
+		},
+		dateIsoStr: "white",
+		filePathWithLine: ["bold", "yellow", "bgYellowBright"],
+		name: ["white"],
+		nameWithDelimiterPrefix: ["white", "bold"],
+		nameWithDelimiterSuffix: ["white", "bold"],
+		errorName: ["bold", "bgRedBright", "whiteBright"],
+		fileName: ["white"],
+		fileNameWithLine: "white",
+	},
+};
+
+export const logger: Logger<ILogObj> = new Logger(
+	Object.assign(defaultOptions, optionLoggers)
+);
+export const important: Logger<ILogObj> = new Logger({
+	name: "Note",
+	minLevel: 0,
+	hideLogPositionForProduction: true,
+	prettyLogTemplate:
+		"{{dd}}/{{mm}}/{{yyyy}} {{hh}}:{{MM}}:{{ss}}.{{ms}} [{{logLevelName}}] ",
+	prettyLogStyles: {
+		dd: "dim",
+		mm: "dim",
+		yyyy: "dim",
+		hh: "dim",
+		// biome-ignore lint/style/useNamingConvention: <explanation>
+		MM: "dim",
+		ss: "dim",
+		ms: "dim",
+		logLevelName: {
+			"*": ["bold", "black", "bgWhiteBright", "dim"],
+			SILLY: ["bold", "white"],
+			TRACE: ["bold", "whiteBright"],
+			DEBUG: ["bold", "green"],
+			INFO: ["bold", "green", "bgGreenBright"],
+			WARN: ["bold", "yellow"],
+			ERROR: ["bold", "red"],
+			FATAL: ["bold", "redBright"],
+		},
+	},
+});
