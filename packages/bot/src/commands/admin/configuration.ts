4381bf8155a06a2f0fb9a65b587dd8d096b7c954
Mara <mara-li@outlook.fr>
2025-02-10 08:43:10 +0100

    refactor(timer): use `min` for displaying minutes in config

--- packages/bot/src/commands/admin/configuration.ts
+++ packages/bot/src/commands/admin/configuration.ts
@@ -492,10 +492,10 @@ function formatDuration(seconds: number) {
 	const minutes = Math.floor(seconds / 60);
 	const remainingSeconds = seconds % 60;
 	if (minutes < 60)
-		return remainingSeconds ? `${minutes}m ${remainingSeconds}s` : `${minutes}m`;
+		return remainingSeconds ? `${minutes}min ${remainingSeconds}s` : `${minutes}min`;
 	const hours = Math.floor(minutes / 60);
 	const remainingMinutes = minutes % 60;
-	return remainingMinutes ? `${hours}h ${remainingMinutes}m` : `${hours}h`;
+	return remainingMinutes ? `${hours}h ${remainingMinutes}min` : `${hours}h`;
 }
 
 async function timer(
