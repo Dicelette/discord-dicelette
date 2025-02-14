08f698a18904712aade1bd270b0ae6540d235690
Mara <mara-li@outlook.fr>
2025-02-14 14:09:23 +0100

    feat(error-handling): improve error reporting with detailed messages

--- packages/bot/src/events/on_error.ts
+++ packages/bot/src/events/on_error.ts
@@ -1,15 +1,33 @@
 import process from "node:process";
-import { logger } from "@dicelette/utils";
 import type { EClient } from "client";
+import dedent from "dedent";
 import dotenv from "dotenv";
 
 dotenv.config({ path: process.env.PROD ? ".env.prod" : ".env" });
 
+function formatErrorMessage(error: unknown): string {
+	if (error instanceof Error) {
+		return dedent(`## ❌ Erreur détectée
+				**Message**: \`${error.message}\`
+				
+				**Stack trace**:
+				\`\`\`
+				${error.stack}
+				\`\`\`
+        `);
+	}
+	return dedent(`## ❌ Erreur inconnue
+			\`\`\`
+			${String(error)}
+			\`\`\`
+  `);
+}
+
 export default (client: EClient): void => {
 	client.on("error", async (error) => {
-		logger.fatal(error);
+		console.error(error);
 		if (!process.env.OWNER_ID) return;
 		const dm = await client.users.createDM(process.env.OWNER_ID);
-		await dm.send(`An error has occurred:\n\`\`\`\n${error.message}\n\`\`\``);
+		await dm.send({ content: formatErrorMessage(error) });
 	});
 };
