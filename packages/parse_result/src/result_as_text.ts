Dicelette/discord-dicelette

commit 5585ab5909d616a9148ae9d3b47b16dbd9716399

feat: allow to register a dice type and roll without statistics & user registered (only if the dice type doesn't contains $)

packages/parse_result/src/result_as_text.ts
@@ -112,7 +112,7 @@ export class ResultAsText {
 		const messageResult = this.resultat.result.split(";");
 		let successOrFailure = "";
 		let isCritical: undefined | "failure" | "success" | "custom" = undefined;
-		if (this.resultat.compare) {
+		if (this.resultat.compare || customCritical) {
 			msgSuccess = "";
 			let total = 0;
 			const natural: number[] = [];
@@ -122,15 +122,22 @@ export class ResultAsText {
 					continue;
 				}
 				const tot = r.match(/ = (\d+)/);
+				let testValue: ComparedValue | undefined = undefined;
+				let goodSign = "";
 				if (tot) {
 					total = Number.parseInt(tot[1], 10);
 				}
-				successOrFailure = evaluate(
-					`${total} ${this.resultat.compare.sign} ${this.resultat.compare.value}`
-				)
-					? `**${this.ul("roll.success")}**`
-					: `**${this.ul("roll.failure")}**`;
+				if (this.resultat.compare) {
+					successOrFailure = evaluate(
+						`${total} ${this.resultat.compare.sign} ${this.resultat.compare.value}`
+					)
+						? `**${this.ul("roll.success")}**`
+						: `**${this.ul("roll.failure")}**`;
+					testValue = this.resultat.compare;
+					goodSign = this.goodCompareSign(testValue, total);
+				}
 				// noinspection RegExpRedundantEscape
+
 				const naturalDice = r.matchAll(/\[(\d+)\]/gi);
 				for (const dice of naturalDice) {
 					natural.push(Number.parseInt(dice[1], 10));
@@ -144,8 +151,7 @@ export class ResultAsText {
 						isCritical = "success";
 					}
 				}
-				let testValue = this.resultat.compare;
-				let goodSign = this.goodCompareSign(testValue, total);
+
 				if (customCritical) {
 					for (const [name, custom] of Object.entries(customCritical)) {
 						const valueToCompare = custom.onNaturalDice ? natural : total;

packages/bot/src/commands/roll/dbd.ts
@@ -1,4 +1,5 @@
 import { cmdLn, t } from "@dicelette/localization";
+import { uniformizeRecords } from "@dicelette/parse_result";
 import { capitalizeBetweenPunct, filterChoices, logger } from "@dicelette/utils";
 import type { EClient } from "client";
 import { getFirstChar, getTemplateWithInteraction, getUserFromMessage } from "database";
@@ -22,17 +23,17 @@ export default {
 			interaction.guild!.id,
 			`user.${interaction.user.id}`
 		);
-		if (!user) return;
+		if (!user && !db.templateID.damageName) return;
 		let choices: string[] = [];
 		if (focused.name === t("rAtq.atq_name.name")) {
 			const char = options.getString(t("common.character"));
 
-			if (char) {
+			if (char && user) {
 				const values = user.find((data) => {
 					return data.charName?.subText(char);
 				});
 				if (values?.damageName) choices = values.damageName;
-			} else {
+			} else if (user) {
 				for (const [, value] of Object.entries(user)) {
 					if (value.damageName) choices = choices.concat(value.damageName);
 				}
@@ -43,7 +44,7 @@ export default {
 				choices.length === 0
 			)
 				choices = choices.concat(db.templateID.damageName);
-		} else if (focused.name === t("common.character")) {
+		} else if (focused.name === t("common.character") && user) {
 			//if dice is set, get all characters that have this dice
 			const skill = options.getString(t("rAtq.atq_name.name"));
 			const allCharactersFromUser = user
@@ -73,7 +74,6 @@ export default {
 				choices = allCharactersFromUser;
 			}
 		}
-		console.debug(`choices: ${choices}`);
 		if (!choices || choices.length === 0) return;
 		const filter = filterChoices(choices, interaction.options.getFocused());
 		await interaction.respond(
@@ -88,10 +88,16 @@ export default {
 		const db = client.settings.get(interaction.guild!.id);
 		if (!db || !interaction.guild || !interaction.channel) return;
 		const user = client.settings.get(interaction.guild.id, `user.${interaction.user.id}`);
-		if (!user) return;
+		const { ul } = getLangAndConfig(client.settings, interaction);
+		if (!user && !db.templateID) {
+			await reply(interaction, {
+				embeds: [embedError(t("error.noUserData"), ul)],
+				flags: Djs.MessageFlags.Ephemeral,
+			});
+			return;
+		}
 		let charOptions = options.getString(t("common.character")) ?? undefined;
 		const charName = charOptions?.normalize();
-		const { ul } = getLangAndConfig(client.settings, interaction);
 		try {
 			let userStatistique = await getUserFromMessage(
 				client,
@@ -112,7 +118,7 @@ export default {
 			}
 			charOptions = userStatistique?.userName ? userStatistique.userName : undefined;
 			if (!userStatistique && !charName) {
-				const char = await getFirstChar(client, interaction, ul);
+				const char = await getFirstChar(client, interaction, ul, true);
 				userStatistique = char?.userStatistique;
 				charOptions = char?.optionChar ?? undefined;
 			}
@@ -142,7 +148,10 @@ export default {
 					});
 					return;
 				}
-				const damage = template.damage;
+				const damage = template.damage
+					? (uniformizeRecords(template.damage) as Record<string, string>)
+					: undefined;
+				logger.trace("The template use:", damage);
 
 				//create the userStatistique with the value got from the template & the commands
 				userStatistique = {

packages/bot/src/commands/roll/dbroll.ts
@@ -24,7 +24,7 @@ export const dbRoll = {
 	async execute(interaction: Djs.CommandInteraction, client: EClient) {
 		const { userStatistique, options, ul, optionChar } =
 			(await getStatistics(interaction, client)) ?? {};
-		if (!userStatistique || !options || !ul || !optionChar) return;
+		if (!userStatistique || !options || !ul) return;
 		return await rollStatistique(
 			interaction,
 			client,

packages/bot/src/commands/tools/graph.ts
@@ -185,7 +185,9 @@ export const graph = {
 		}
 		const serverTemplate = await getTemplateWithInteraction(interaction, client);
 		if (!guildData.templateID.statsName || !serverTemplate?.statistics) {
-			await reply(interaction, { embeds: [embedError(ul("error.noStats"), ul)] });
+			await reply(interaction, {
+				embeds: [embedError(ul("error.stats.notFound_plural"), ul)],
+			});
 			return;
 		}
 		const user = options.getUser(t("display.userLowercase"));

packages/bot/src/database/get_user.ts
@@ -16,7 +16,7 @@ import type {
 } from "@dicelette/types";
 import { cleanAvatarUrl, logger } from "@dicelette/utils";
 import type { EClient } from "client";
-import { getCharaInMemory, updateMemory } from "database";
+import { getCharaInMemory, getTemplateWithInteraction, updateMemory } from "database";
 import * as Djs from "discord.js";
 import { embedError, ensureEmbed, getEmbeds, reply } from "messages";
 import { getLangAndConfig, haveAccess, searchUserChannel, serializeName } from "utils";
@@ -62,19 +62,22 @@ export function getUserByEmbed(
 export async function getFirstChar(
 	client: EClient,
 	interaction: Djs.CommandInteraction,
-	ul: Translation
+	ul: Translation,
+	skipNotFound = false
 ) {
 	const userData = client.settings.get(
 		interaction.guild!.id,
 		`user.${interaction.user.id}`
 	);
 	if (!userData) {
+		if (skipNotFound) return;
 		await reply(interaction, {
 			embeds: [embedError(ul("error.notRegistered"), ul)],
 			flags: Djs.MessageFlags.Ephemeral,
 		});
 		return;
 	}
+
 	const firstChar = userData[0];
 	const optionChar = firstChar.charName?.capitalize();
 	const userStatistique = await getUserFromMessage(
@@ -337,11 +340,25 @@ export async function getStatistics(
 		return;
 	}
 	optionChar = userStatistique?.userName ? userStatistique.userName : undefined;
-	if (!userStatistique && !charName) {
+	const template = await getTemplateWithInteraction(interaction, client);
+	const needStats = template?.diceType?.includes("$");
+	if (!userStatistique && !charName && needStats) {
 		//find the first character registered
 		const char = await getFirstChar(client, interaction, ul);
 		userStatistique = char?.userStatistique;
 		optionChar = char?.optionChar;
+	}
+
+	if (!needStats && !userStatistique) {
+		//we can use the dice without an user i guess
+		userStatistique = {
+			template: {
+				diceType: template?.diceType,
+				critical: template?.critical,
+				customCritical: template?.customCritical,
+			},
+			damage: template?.damage,
+		};
 	}
 	if (!userStatistique) {
 		await reply(interaction, {
@@ -351,7 +368,7 @@ export async function getStatistics(
 		return;
 	}
 
-	if (!userStatistique.stats) {
+	if (!userStatistique.stats && !template?.statistics && needStats) {
 		await reply(interaction, {
 			embeds: [embedError(ul("error.noStats"), ul)],
 			flags: Djs.MessageFlags.Ephemeral,
@@ -386,7 +403,7 @@ export function getRightValue(
 		}
 		if (userStat) break;
 		throw new Error(
-			ul("error.noStat", {
+			ul("error.stats.notFound_singular", {
 				stat: standardizedStatistic.capitalize(),
 				char: optionChar ? ` ${optionChar.capitalize()}` : "",
 			})

packages/bot/src/features/stats/modals.ts
@@ -153,7 +153,7 @@ export async function editStats(
 			embedsStatsFields.find((field) => field.name.unidecode() === name.unidecode())
 		)
 			continue;
-		if (!stat) throw new Error(ul("error.statNotFound", { value: name }));
+		if (!stat) throw new Error(ul("error.stats.notFound", { value: name }));
 
 		if (!isNumber(value)) {
 			//it's a combinaison OR an error

packages/bot/src/utils/commands_options.ts
@@ -97,7 +97,7 @@ export function dbRollOptions(
 			.setNameLocalizations(cmdLn("common.statistic"))
 			.setDescription(t("dbRoll.options.statistic"))
 			.setDescriptionLocalizations(cmdLn("dbRoll.options.statistic"))
-			.setRequired(true)
+			.setRequired(false)
 			.setAutocomplete(true)
 	);
 	return commonOptions(builder, { expression: true, threshold: true });

packages/bot/src/utils/roll.ts
@@ -5,18 +5,18 @@ import {
 } from "@dicelette/core";
 import { t } from "@dicelette/localization";
 import {
-	ResultAsText,
-	type Server,
 	convertExpression,
 	convertNameToValue,
 	getRoll,
+	ResultAsText,
 	replaceStatInDice,
 	rollCustomCritical,
+	type Server,
 	skillCustomCritical,
 	trimAll,
 } from "@dicelette/parse_result";
 import type { Settings, Translation, UserData } from "@dicelette/types";
-import { capitalizeBetweenPunct } from "@dicelette/utils";
+import { capitalizeBetweenPunct, logger } from "@dicelette/utils";
 import type { EClient } from "client";
 import { getRightValue } from "database";
 import * as Djs from "discord.js";
@@ -82,6 +82,8 @@ export async function rollDice(
 		standardized: atq.standardize(),
 	};
 	atq = atq.standardize();
+	const expression = options.getString(t("dbRoll.options.modificator.name")) ?? "0";
+	logger.trace("ATQ USED", atq);
 	const comm = options.getString(t("dbRoll.options.comments.name"))
 		? `# ${options.getString(t("dbRoll.options.comments.name"))}`
 		: undefined;
@@ -118,8 +120,17 @@ export async function rollDice(
 		return;
 	}
 	const dollarValue = convertNameToValue(atq, userStatistique.stats);
+	let expressionStr = convertExpression(
+		expression,
+		userStatistique.stats,
+		dollarValue?.total
+	);
+	if (dice.includes("{exp}")) {
+		if (expression === "0") dice = dice.replaceAll("{exp}", "1");
+		else dice = dice.replaceAll("{exp}", `${expressionStr.replace(/^\+/, "")}`);
+		expressionStr = "";
+	}
 	dice = generateStatsDice(dice, userStatistique.stats, dollarValue?.total);
-	const expression = options.getString(t("dbRoll.options.modificator.name")) ?? "0";
 	const comparatorMatch = /(?<sign>[><=!]+)(?<comparator>(.+))/.exec(dice);
 	let comparator = "";
 	if (comparatorMatch) {
@@ -138,17 +149,9 @@ export async function rollDice(
 		else infoRoll.name = replaceStatInDice(infoRoll.name, userStatistique.stats, "");
 		if (infoRoll.name.length === 0) infoRoll.name = capitalizeBetweenPunct(originalName);
 	}
+
 	comparator = generateStatsDice(comparator, userStatistique.stats, dollarValue?.total);
-	let expressionStr = convertExpression(
-		expression,
-		userStatistique.stats,
-		dollarValue?.total
-	);
-	if (dice.includes("{exp}")) {
-		if (expression === "0") dice = dice.replaceAll("{exp}", "1");
-		else dice = dice.replaceAll("{exp}", `${expressionStr.replace(/^\+/, "")}`);
-		expressionStr = "";
-	}
+
 	const roll = `${trimAll(dice)}${expressionStr}${comparator} ${comments}`;
 	await rollWithInteraction(
 		interaction,
@@ -177,15 +180,24 @@ export async function rollStatistique(
 	user?: Djs.User,
 	hideResult?: boolean | null
 ) {
-	let statistic = options.getString(t("common.statistic"), true);
-	let standardizedStatistic = statistic.standardize(true);
+	let statistic = options.getString(t("common.statistic"), false);
+	const template = userStatistique.template;
+	let dice = template.diceType;
+	let standardizedStatistic = statistic?.standardize(true);
 	//return if the standardizedStatistic is excluded from the list
 	const excludedStats = client.settings
 		.get(interaction.guild!.id, "templateID.excludedStats")
 		?.map((stat) => stat.standardize());
-	if (excludedStats?.includes(standardizedStatistic)) {
+	if (standardizedStatistic && excludedStats?.includes(standardizedStatistic)) {
 		await reply(interaction, {
-			content: ul("error.excludedStat"),
+			content: ul("error.stats.excluded"),
+			flags: Djs.MessageFlags.Ephemeral,
+		});
+		return;
+	}
+	if (template.diceType?.includes("$") && !statistic) {
+		await reply(interaction, {
+			content: ul("error.stats.shouldSelect"),
 			flags: Djs.MessageFlags.Ephemeral,
 		});
 		return;
@@ -196,23 +208,25 @@ export async function rollStatistique(
 		: undefined;
 	const comments = comm ?? "";
 	const override = options.getString(t("dbRoll.options.override.name"));
+	let userStat: undefined | number = undefined;
 	const modification = options.getString(t("dbRoll.options.modificator.name")) ?? "0";
+	if (statistic && standardizedStatistic && dice?.includes("$")) {
+		const res = getRightValue(
+			userStatistique,
+			standardizedStatistic,
+			ul,
+			client,
+			interaction,
+			optionChar,
+			statistic
+		);
+		if (!res) return;
+		statistic = res.statistic;
+		standardizedStatistic = res.standardizedStatistic;
+		userStat = res.userStat;
 
-	const res = getRightValue(
-		userStatistique,
-		standardizedStatistic,
-		ul,
-		client,
-		interaction,
-		optionChar,
-		statistic
-	);
-	if (!res) return;
-	statistic = res.statistic;
-	standardizedStatistic = res.standardizedStatistic;
-	const userStat = res.userStat;
-	const template = userStatistique.template;
-	let dice = template.diceType?.replaceAll("$", userStat.toString());
+		dice = dice.replaceAll("$", userStat.toString());
+	}
 	if (!dice) {
 		await reply(interaction, {
 			content: ul("error.noDice"),
@@ -234,7 +248,7 @@ export async function rollStatistique(
 	const modificationString = convertExpression(
 		modification,
 		userStatistique.stats,
-		userStat.toString()
+		userStat?.toString()
 	);
 	const comparatorMatch = /(?<sign>[><=!]+)(?<comparator>(.+))/.exec(dice);
 	let comparator = "";
@@ -243,10 +257,14 @@ export async function rollStatistique(
 		dice = dice.replace(comparatorMatch[0], "").trim();
 		comparator = comparatorMatch[0];
 	}
-	const roll = `${trimAll(replaceFormulaInDice(dice))}${modificationString}${generateStatsDice(comparator, userStatistique.stats, userStat.toString())} ${comments}`;
+	const roll = `${trimAll(replaceFormulaInDice(dice))}${modificationString}${generateStatsDice(comparator, userStatistique.stats, userStat?.toString())} ${comments}`;
 	const customCritical = template.customCritical
 		? rollCustomCritical(template.customCritical, userStat, userStatistique.stats)
 		: undefined;
+	const infoRoll =
+		statistic && standardizedStatistic
+			? { name: statistic, standardized: standardizedStatistic }
+			: undefined;
 	await rollWithInteraction(
 		interaction,
 		roll,
@@ -254,7 +272,7 @@ export async function rollStatistique(
 		template.critical,
 		user,
 		optionChar,
-		{ name: statistic, standardized: standardizedStatistic },
+		infoRoll,
 		hideResult,
 		customCritical
 	);

commit f8c0505b924502019e4386c468024d292361fa25

refactor: regression from 5585ab5909d616a9148ae9d3b47b16dbd9716399 that can make entering in a non wanted custom critical
Added docs for future so i don't forget the "condition" to force a custom critical (dice>=1 can works)

I probably confound the goal of **critical** and the "message" for fail/success.

packages/parse_result/src/result_as_text.ts
@@ -112,7 +112,12 @@ export class ResultAsText {
 		const messageResult = this.resultat.result.split(";");
 		let successOrFailure = "";
 		let isCritical: undefined | "failure" | "success" | "custom" = undefined;
-		if (this.resultat.compare || customCritical) {
+		/**
+		 * @warning Do not forget that the compare value is needed **EVEN while using** custom critical.
+		 *
+		 * So a dice with CustomCritical can be set like `1d100>=1` (always true) to enter in the condition & CC loop.
+		 */
+		if (this.resultat.compare) {
 			msgSuccess = "";
 			let total = 0;
 			const natural: number[] = [];
@@ -122,22 +127,15 @@ export class ResultAsText {
 					continue;
 				}
 				const tot = r.match(/ = (\d+)/);
-				let testValue: ComparedValue | undefined = undefined;
-				let goodSign = "";
 				if (tot) {
 					total = Number.parseInt(tot[1], 10);
 				}
-				if (this.resultat.compare) {
-					successOrFailure = evaluate(
-						`${total} ${this.resultat.compare.sign} ${this.resultat.compare.value}`
-					)
-						? `**${this.ul("roll.success")}**`
-						: `**${this.ul("roll.failure")}**`;
-					testValue = this.resultat.compare;
-					goodSign = this.goodCompareSign(testValue, total);
-				}
+				successOrFailure = evaluate(
+					`${total} ${this.resultat.compare.sign} ${this.resultat.compare.value}`
+				)
+					? `**${this.ul("roll.success")}**`
+					: `**${this.ul("roll.failure")}**`;
 				// noinspection RegExpRedundantEscape
-
 				const naturalDice = r.matchAll(/\[(\d+)\]/gi);
 				for (const dice of naturalDice) {
 					natural.push(Number.parseInt(dice[1], 10));
@@ -151,7 +149,8 @@ export class ResultAsText {
 						isCritical = "success";
 					}
 				}
-
+				let testValue = this.resultat.compare;
+				let goodSign = this.goodCompareSign(testValue, total);
 				if (customCritical) {
 					for (const [name, custom] of Object.entries(customCritical)) {
 						const valueToCompare = custom.onNaturalDice ? natural : total;
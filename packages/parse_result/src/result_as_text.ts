f8c0505b924502019e4386c468024d292361fa25
Mara <mara-li@outlook.fr>
2025-05-08 02:30:22 +0200

    refactor: regression from 5585ab5909d616a9148ae9d3b47b16dbd9716399 that can make entering in a non wanted custom critical
    Added docs for future so i don't forget the "condition" to force a custom critical (dice>=1 can works)
    
    I probably confound the goal of **critical** and the "message" for fail/success.

--- packages/parse_result/src/result_as_text.ts
+++ packages/parse_result/src/result_as_text.ts
@@ -112,7 +112,12 @@ export class ResultAsText {
 		const messageResult = this.resultat.result.split(";");
 		let successOrFailure = "";
 		let isCritical: undefined | "failure" | "success" | "custom" = undefined;
-		if (this.resultat.compare || customCritical) {
+		/**
+		 * @warning Do not forget that the compare value is needed **EVEN while using** custom critical.
+		 *
+		 * So a dice with CustomCritical can be set like `1d100>=1` (always true) to enter in the condition & CC loop.
+		 */
+		if (this.resultat.compare) {
 			msgSuccess = "";
 			let total = 0;
 			const natural: number[] = [];
@@ -122,22 +127,15 @@ export class ResultAsText {
 					continue;
 				}
 				const tot = r.match(/ = (\d+)/);
-				let testValue: ComparedValue | undefined = undefined;
-				let goodSign = "";
 				if (tot) {
 					total = Number.parseInt(tot[1], 10);
 				}
-				if (this.resultat.compare) {
-					successOrFailure = evaluate(
-						`${total} ${this.resultat.compare.sign} ${this.resultat.compare.value}`
-					)
-						? `**${this.ul("roll.success")}**`
-						: `**${this.ul("roll.failure")}**`;
-					testValue = this.resultat.compare;
-					goodSign = this.goodCompareSign(testValue, total);
-				}
+				successOrFailure = evaluate(
+					`${total} ${this.resultat.compare.sign} ${this.resultat.compare.value}`
+				)
+					? `**${this.ul("roll.success")}**`
+					: `**${this.ul("roll.failure")}**`;
 				// noinspection RegExpRedundantEscape
-
 				const naturalDice = r.matchAll(/\[(\d+)\]/gi);
 				for (const dice of naturalDice) {
 					natural.push(Number.parseInt(dice[1], 10));
@@ -151,7 +149,8 @@ export class ResultAsText {
 						isCritical = "success";
 					}
 				}
-
+				let testValue = this.resultat.compare;
+				let goodSign = this.goodCompareSign(testValue, total);
 				if (customCritical) {
 					for (const [name, custom] of Object.entries(customCritical)) {
 						const valueToCompare = custom.onNaturalDice ? natural : total;
