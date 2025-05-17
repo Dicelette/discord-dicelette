2fd0a1a328693335a8dddfdfc8bc073a51340ac2
Mara <mara-li@outlook.fr>
2025-05-17 08:25:52 +0200

    fix(dev): add error handling for loading testing files

--- packages/utils/src/dev.ts
+++ packages/utils/src/dev.ts
@@ -11,17 +11,21 @@ import { logger } from "./logger";
 const TESTING_DIRECTORY = "@testing";
 
 export default function (template: Enmap<string, StatisticalTemplate, unknown>) {
-	const allFiles = fs.readdirSync(TESTING_DIRECTORY);
-	for (const file of allFiles) {
-		if (file.endsWith(".json")) {
-			const filePath = path.resolve(TESTING_DIRECTORY, file);
-			const data = fs.readFileSync(filePath, "utf-8");
-			//as we are in testing, we assume the given file is a valid json
-			const json = JSON.parse(data) as StatisticalTemplate;
-			const guildId = path.basename(filePath, ".json");
-			logger.debug(`Loading guild: ${guildId}`);
-			template.set(guildId, json);
+	try {
+		const allFiles = fs.readdirSync(TESTING_DIRECTORY);
+		for (const file of allFiles) {
+			if (file.endsWith(".json")) {
+				const filePath = path.resolve(TESTING_DIRECTORY, file);
+				const data = fs.readFileSync(filePath, "utf-8");
+				//as we are in testing, we assume the given file is a valid json
+				const json = JSON.parse(data) as StatisticalTemplate;
+				const guildId = path.basename(filePath, ".json");
+				logger.debug(`Loading guild: ${guildId}`);
+				template.set(guildId, json);
+			}
 		}
+	} catch (e) {
+		logger.error("Error loading testing files: ", e);
 	}
 	return template;
 }
