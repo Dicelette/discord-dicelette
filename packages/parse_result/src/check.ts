ec187488dbff23130103f5e38f78fd23fda769aa
Mara <mara-li@outlook.fr>
2025-06-08 19:37:15 +0200

    fix(chainedRoll): better take into account the comments on_message_send.ts

--- packages/parse_result/src/check.ts
+++ packages/parse_result/src/check.ts
@@ -3,16 +3,20 @@ import { DETECT_DICE_MESSAGE } from "./interfaces.js";
 import { trimAll } from "./utils";
 
 export function isRolling(content: string) {
-	const detectRoll = content.match(/\[(.*)\]/)?.[1];
-	const comments = content.match(DETECT_DICE_MESSAGE)?.[3].replaceAll("*", "\\*");
+	const detectRoll = content.match(/\[[^:]+?\]/)?.[1];
+	let comments = content.match(DETECT_DICE_MESSAGE)?.[3].replaceAll("*", "\\*");
 	if (comments && !detectRoll) {
 		const diceValue = content.match(/^\S*#?d\S+|\{.*\}/i);
 		if (!diceValue) return;
-		content = content.replace(DETECT_DICE_MESSAGE, "$1");
+		const chained = chainedComments(content, comments);
+		content = chained.content;
+		comments = chained.comments;
 	}
 	let result: Resultat | undefined;
 	try {
-		result = detectRoll ? roll(trimAll(detectRoll)) : roll(trimAll(content));
+		result = detectRoll
+			? roll(trimAll(detectRoll))
+			: roll(trimAll(content.replace(/\[\/{2}(.*)\]/, "[$1]")));
 	} catch (e) {
 		return undefined;
 	}
@@ -21,4 +25,27 @@ export function isRolling(content: string) {
 		result.comment = comments;
 	}
 	return { result, detectRoll };
+}
+
+function chainedComments(content: string, comments: string) {
+	if (
+		comments.match(/\[(.*)]/) &&
+		content.includes("&") &&
+		content.includes(";")
+	) {
+		//we must search for the global comments, that will start as `# comments`
+		const globalComments = content.match(/# ?(.*)/)?.[1];
+		//remove from the content the comments
+		if (globalComments) {
+			content = content.replace(/# ?(.*)/, "").trim();
+			return { content, comments: globalComments };
+		}
+		return {
+			content,
+			comments: undefined,
+		};
+	}
+	return {
+		content: content.replace(DETECT_DICE_MESSAGE, "$1"),
+	};
 }
