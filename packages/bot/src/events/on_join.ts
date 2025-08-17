3ec115b7cda5d86dfd408c662f5172add9d32b9b
Mara <mara-li@outlook.fr>
2025-08-17 11:19:50 +0200

    perf: improve join

--- packages/bot/src/events/on_join.ts
+++ packages/bot/src/events/on_join.ts
@@ -9,18 +9,15 @@ export default (client: EClient): void => {
 			client.template.set(guild.id, {});
 			client.settings.set(guild.id, true, "converted");
 			client.settings.set(guild.id, true, "disableThread");
-			await helpAtInvit(guild);
-			const commandPromises = commandsList.map(async (command) => {
-				await guild.commands.create(command.data);
-				logger.trace(`Command ${command.data.name} created in ${guild.name}`);
-				return command.data.name;
-			});
 
-			const contextMenuPromises = contextMenus.map(async (contextMenu) => {
-				await guild.commands.create(contextMenu);
-				return contextMenu.name;
-			});
-			await Promise.all([...commandPromises, ...contextMenuPromises]);
+			const allCommands = [
+				...commandsList.map((command) => command.data),
+				...contextMenus,
+			];
+
+			await guild.commands.set(allCommands);
+
+			await helpAtInvit(guild);
 		} catch (e) {
 			logger.fatal(e);
 		}
