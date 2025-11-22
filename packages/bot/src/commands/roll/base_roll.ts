e3f9345037eaf49302d8e3eaf5ecbdbf244ce4fd
Mara <mara-li@outlook.fr>
2025-11-22 18:49:33 +0100

    refactor: refactor baseRoll function for improved readability and functionality

--- packages/bot/src/commands/roll/base_roll.ts
+++ packages/bot/src/commands/roll/base_roll.ts
@@ -32,50 +32,62 @@ export const diceRoll = {
 		client: EClient
 	): Promise<void> {
 		const option = interaction.options as Djs.CommandInteractionOptionResolver;
-		let dice = option.getString(t("common.dice"), true);
+		const dice = option.getString(t("common.dice"), true);
 		const hidden = option.getBoolean(t("dbRoll.options.hidden.name"));
-		const { ul } = getLangAndConfig(client, interaction);
-		let firstChara: string | undefined;
-		if (dice.match(/\$([a-zA-Z_][a-zA-Z0-9_]*)/) && interaction.guild)
-			firstChara = await getCharFromText(
-				client,
-				interaction.guild.id,
-				interaction.user.id,
-				dice
-			);
-		if (firstChara) dice = dice.replace(/ @\w+/, "").trim();
-		const data = interaction.guild
-			? await getUserFromInteraction(
-					client,
-					interaction.user.id,
-					interaction,
-					firstChara,
-					{ skipNotFound: true }
-				)
-			: undefined;
-		const userData = data?.userData;
-		const charName = data?.charName ?? firstChara;
-		const res = replaceStatsInDiceFormula(dice, userData?.stats, true);
-		const opposition = parseComparator(dice, userData?.stats, res.infoRoll);
-		const { criticalsFromDice, serverData } = await getCritical(
-			client,
-			ul,
-			res.formula,
-			interaction.guild || undefined,
-			userData,
-			rollCustomCriticalsFromDice(dice, ul)
-		);
-		await rollWithInteraction(
-			interaction,
-			res.formula,
-			client,
-			serverData?.critical,
-			undefined,
-			charName,
-			undefined,
-			hidden,
-			criticalsFromDice,
-			opposition
-		);
+		await baseRoll(dice, interaction, client, hidden ?? undefined);
 	},
 };
+
+export async function baseRoll(
+	dice: string,
+	interaction: Djs.ChatInputCommandInteraction,
+	client: EClient,
+	hidden?: boolean,
+	silent?: boolean
+): Promise<void> {
+	const { ul } = getLangAndConfig(client, interaction);
+	let firstChara: string | undefined;
+	if (dice.match(/\$([a-zA-Z_][a-zA-Z0-9_]*)/) && interaction.guild)
+		firstChara = await getCharFromText(
+			client,
+			interaction.guild.id,
+			interaction.user.id,
+			dice
+		);
+	if (firstChara) dice = dice.replace(/ @\w+/, "").trim();
+
+	const data = interaction.guild
+		? await getUserFromInteraction(client, interaction.user.id, interaction, firstChara, {
+				skipNotFound: true,
+			})
+		: undefined;
+	const userData = data?.userData;
+	let charName = data?.charName ?? firstChara;
+	if (!charName && dice.match(/ @\w+/)) {
+		charName = dice.match(/ @(\w+)/)![1];
+		dice = dice.replace(/ @\w+/, "").trim();
+	}
+	const res = replaceStatsInDiceFormula(dice, userData?.stats, true);
+	const opposition = parseComparator(dice, userData?.stats, res.infoRoll);
+	const { criticalsFromDice, serverData } = await getCritical(
+		client,
+		ul,
+		res.formula,
+		interaction.guild || undefined,
+		userData,
+		rollCustomCriticalsFromDice(dice, ul)
+	);
+	await rollWithInteraction(
+		interaction,
+		res.formula,
+		client,
+		serverData?.critical,
+		undefined,
+		charName,
+		undefined,
+		hidden,
+		criticalsFromDice,
+		opposition,
+		silent
+	);
+}
