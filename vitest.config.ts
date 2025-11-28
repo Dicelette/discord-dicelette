cf1d53df950a512a2856d6da789cfd2ad537f99d
Mara <mara-li@outlook.fr>
2025-11-28 14:29:51 +0100

    fix config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,14 +4,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		alias: {
-			"@dicelette/localization": path.resolve(
-				__dirname,
-				"packages/localization/index.ts"
-			),
 			"@dicelette/types": path.resolve(__dirname, "packages/types/index.ts"),
 			"@dicelette/utils": path.resolve(__dirname, "packages/utils/index.ts"),
 		},
-		projects: ["packages/*"],
 		exclude: [],
+		projects: ["packages/*"],
 	},
 });
