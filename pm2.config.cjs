533a99d7aff643662b28f07cabd2fb19787a4b1c
Mara <30244939+Mara-Li@users.noreply.github.com>
2026-06-25 23:45:38 +0200

    chore: use pm2 config file + increase max old space size + max memory

---
+++ pm2.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+	name: "dicelette",
+	script: "dist/apps/bot/index.js",
+	node_args: "--max-old-space-size=1024",
+	max_memory_restart: "900M",
+	log_date_format: "YYYY-MM-DD HH:mm Z",
+	time: true,
+	interpreter: "node",
+};
