f2ce63c901de9b435e4bfc167e5d766e22df2f91
Mara <30244939+Mara-Li@users.noreply.github.com>
2026-06-27 10:55:04 +0200

    chore: update pm2 config

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,12 +1,16 @@
 module.exports = {
-	name: "dicelette",
-	script: "dist/apps/bot/index.js",
-	node_args: "--max-old-space-size=1024",
-	max_memory_restart: "900M",
-	log_date_format: "YYYY-MM-DD HH:mm Z",
-	time: true,
-	interpreter: "node",
-	env: {
-		NODE_OPTIONS: "--max-old-space-size=1024",
-	},
+	apps: [
+		{
+			name: "dicelette",
+			script: "dist/apps/bot/index.js",
+			node_args: "--max-old-space-size=1024",
+			max_memory_restart: "900M",
+			log_date_format: "YYYY-MM-DD HH:mm Z",
+			time: true,
+			interpreter: "node",
+			env: {
+				NODE_OPTIONS: "--max-old-space-size=1024",
+			},
+		},
+	],
 };
