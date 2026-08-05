e11e30b994211aab5616f7e08f17cd02b025b639
Mara-Li <30244939+Mara-Li@users.noreply.github.com>
2026-08-05 09:00:01 +0200

    refactor: remove useless logs

--- apps/bot/src/events/on_warn.ts
+++ apps/bot/src/events/on_warn.ts
@@ -8,16 +8,6 @@ export const onWarn = (client: EClient): void => {
 	});
 };
 
-export const onDebug = (client: EClient): void => {
-	client.on("debug", async (message) => {
-		const excludedKeys = ["Heartbeat acknowledged, latency of"];
-		if (!excludedKeys.some((key) => message.includes(key))) {
-			important.debug(message);
-			//sentry.debug(message, { source: "discord-debug" });
-		}
-	});
-};
-
 export const shardDebug = (client: EClient): void => {
 	client.on("shardError", async (shardId, message) => {
 		important.error(`${shardId} has encountered an error: ${message}`);
