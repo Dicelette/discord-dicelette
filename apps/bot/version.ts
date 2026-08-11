4b0381bebb1b32a98a9d0d8d7885675d56a2a56d
Mara-Li <30244939+Mara-Li@users.noreply.github.com>
2026-08-11 15:11:44 +0200

    refactor: remove uncessary @ts-expect-error directive

--- apps/bot/version.ts
+++ apps/bot/version.ts
@@ -1,6 +1,5 @@
 import packageJson from "../../package.json" with { type: "json" };
 
-//@ts-expect-error
 export const VERSION = packageJson.version ?? "/";
 export const PRIVATE_ID = (process.env.PRIVATE_ID ?? "453162143668371456")
 	.split(",")
